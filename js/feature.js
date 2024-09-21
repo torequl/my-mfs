da3659ded12c874c1f368e19bd9e125e38122439
torequl <torequlmkt@gmail.com>
2024-09-22 03:25:38 +0600

    ok

--- js/feature.js
+++ js/feature.js
@@ -1,15 +1,26 @@
-// cash-out-form
-// add-money-form
-// add-money-button
-// cash-out-button
+function showSectionByID(id){
+    document.getElementById('add-money-form').classList.add('hidden');
+    document.getElementById('cash-out-form').classList.add('hidden');
+    document.getElementById('transaction-history').classList.add('hidden');
+
+    document.getElementById(id).classList.remove('hidden');
+}
+
+
 
 document.getElementById('add-money-button').addEventListener('click', function(){
-    document.getElementById('add-money-form').classList.remove('hidden');
-    document.getElementById('cash-out-form').classList.add('hidden');
+    showSectionByID('add-money-form');
 })
 
 document.getElementById('cash-out-button').addEventListener('click', function(){
-    document.getElementById('cash-out-form').classList.remove('hidden');
-    document.getElementById('add-money-form').classList.add('hidden');
+    showSectionByID('cash-out-form');
 })
+
+document.getElementById('transaction-btn').addEventListener('click', function(){
+    showSectionByID('transaction-history');
+})
+
+
+
+
 
